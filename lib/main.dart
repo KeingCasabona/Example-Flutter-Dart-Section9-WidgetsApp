69e4426a9c9675d53878a55e8a88eed6495fa23a
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-03-07 15:32:57 -0500

    feat: Add theme and style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,13 +10,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 2).getTheme(),
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: FilledButton(onPressed: () {}, child: Text('Hola mundo')),
         ),
       ),
     );
