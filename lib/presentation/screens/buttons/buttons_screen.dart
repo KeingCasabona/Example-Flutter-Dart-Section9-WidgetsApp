556a55c5942a1b07dc7f745a1fb3b74e681f9dbb
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-04-05 19:30:25 -0500

    feat: Add custom button

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -87,8 +87,37 @@ class _ButtonsView extends StatelessWidget {
                 backgroundColor: MaterialStatePropertyAll(colors.primary),
                 iconColor: MaterialStatePropertyAll(Colors.white),
               ),
-            )
+            ),
+            CustomButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'Hola mundo',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
     );
